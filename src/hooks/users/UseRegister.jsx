67bceaf3a0d03b38bc0ef92fd191e6d9f76f6c07
Jeko <jeko.erg@gamil.com>
2025-02-14 19:26:01 +0400

    minor fixes

--- src/hooks/users/UseRegister.jsx
+++ src/hooks/users/UseRegister.jsx
@@ -3,7 +3,10 @@ import { API } from "../../api/Api";
 
 function UseRegister() {
   const handleRegister = async (params) => {
-    await API.post("/api/user", params,{headers: {"Content-Type":"multipart/form-data"}}).then((res) => {
+    await API.post("/api/user", params, {
+      headers: { "Content-Type": "multipart/form-data" },
+      withCredentials: true,
+    }).then((res) => {
       console.log(res);
     });
   };
